35acb5722698ea5c33fd640482df0b2611cbc98e
XV18 <velasquezx991@gmail.com>
2026-04-23 08:14:36 -0500

    Parte 1: TimerTask extiende Thread

--- src/main/java/ec/edu/utpl/carreras/computacion/proava/MultiTimerConsole.java
+++ src/main/java/ec/edu/utpl/carreras/computacion/proava/MultiTimerConsole.java
@@ -3,10 +3,15 @@ package ec.edu.utpl.carreras.computacion.proava;
 /**
  * Hello world!
  */
+
 public class MultiTimerConsole {
     public static void main(String[] args) {
         TimerTask timerA = new TimerTask("Temporizador A", 8);
-        timerA.run();
-        System.out.println();
+        TimerTask timerB = new TimerTask("Temporizador B", 5);
+        TimerTask timerC = new TimerTask("Temporizador C", 3);
+
+        timerA.start();
+        timerB.start();
+        timerC.start();
     }
 }
